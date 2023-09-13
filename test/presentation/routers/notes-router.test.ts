fac4c3817cae33569356e1b841a682a9b2b62e56
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-13 18:38:35 +0000

    14 Sept 2023, 00:08

---
+++ test/presentation/routers/notes-router.test.ts
@@ -0,0 +1,12 @@
+import CreateNoteUseCase from '../../../src/domain/interfaces/use-cases/notes/create-note-use-case';
+import GetAllNotesUseCase from '../../../src/domain/interfaces/use-cases/notes/get-all-notes-use-case';
+import GetOneNotesUseCase from '../../../src/domain/interfaces/use-cases/notes/get-one-note-use-case';
+import UpdateNoteUseCase from '../../../src/domain/interfaces/use-cases/notes/update-note-use-case';
+
+describe('Note Router', () => {
+  let mockDeleteNoteUseCase: DeleteNoteUseCase;
+  let mockCreateNoteUseCase: CreateNoteUseCase;
+  let mockUpdateNoteUseCase: UpdateNoteUseCase;
+  let mockGelAllNotesUseCase: GetAllNotesUseCase;
+  let mockGetOneNoteUseCase: GetOneNotesUseCase;
+});
