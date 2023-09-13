316301caeec41ca9b8faa9acfe8b2ba1d9acb04a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-13 20:29:34 +0000

    14 Sept 2023, 01:59

---
+++ test/helpers/notesRouterHelper.ts
@@ -0,0 +1,47 @@
+import CreateNoteUseCase from '../../src/domain/interfaces/use-cases/notes/create-note-use-case';
+import DeleteNoteUseCase from '../../src/domain/interfaces/use-cases/notes/delete-note-use-case';
+import GetAllNotesUseCase from '../../src/domain/interfaces/use-cases/notes/get-all-notes-use-case';
+import GetOneNotesUseCase from '../../src/domain/interfaces/use-cases/notes/get-one-note-use-case';
+import UpdateNoteUseCase from '../../src/domain/interfaces/use-cases/notes/update-note-use-case';
+import {
+  NotesRequestModel,
+  NotesResponseModel,
+} from '../../src/domain/models/notes';
+
+class MockCreateNoteUseCase implements CreateNoteUseCase {
+  execute(note: NotesRequestModel): Promise<NotesResponseModel> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+class MockDeleteNoteUseCase implements DeleteNoteUseCase {
+  execute(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+class MockUpdateNoteUseCase implements UpdateNoteUseCase {
+  expect(id: string, note: NotesRequestModel): Promise<NotesResponseModel> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+class MockGetOneNoteUseCase implements GetOneNotesUseCase {
+  execute(id: string): Promise<NotesResponseModel | null> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+class MockGetAllNotesUseCase implements GetAllNotesUseCase {
+  execute(): Promise<NotesResponseModel[]> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export const getMockGetOneNoteUseCase = () => {
+  new MockGetOneNoteUseCase();
+};
+
+export const getMockGetAllNotesUseCase = () => {
+  new MockGetAllNotesUseCase();
+};
