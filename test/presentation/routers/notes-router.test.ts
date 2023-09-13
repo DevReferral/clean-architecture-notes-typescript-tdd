ee43851f5745b7fb7c764c7efdef80adcf70ee0a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-13 20:34:31 +0000

    14 Sept 2023, 02:04

--- test/presentation/routers/notes-router.test.ts
+++ test/presentation/routers/notes-router.test.ts
@@ -3,17 +3,27 @@ import DeleteNoteUseCase from '../../../src/domain/interfaces/use-cases/notes/de
 import GetAllNotesUseCase from '../../../src/domain/interfaces/use-cases/notes/get-all-notes-use-case';
 import GetOneNotesUseCase from '../../../src/domain/interfaces/use-cases/notes/get-one-note-use-case';
 import UpdateNoteUseCase from '../../../src/domain/interfaces/use-cases/notes/update-note-use-case';
-import { getMockDeleteNoteUseCase } from '../../helpers/notesRouterHelper';
+import {
+  getMockCreateNoteUseCase,
+  getMockDeleteNoteUseCase,
+  getMockGetAllNotesUseCase,
+  getMockGetOneNoteUseCase,
+  getMockUpdateNoteUseCase,
+} from '../../helpers/notesRouterHelper';
 
 describe('Note Router', () => {
   let mockDeleteNoteUseCase: DeleteNoteUseCase;
   let mockCreateNoteUseCase: CreateNoteUseCase;
   let mockUpdateNoteUseCase: UpdateNoteUseCase;
-  let mockGelAllNotesUseCase: GetAllNotesUseCase;
+  let mockGetAllNotesUseCase: GetAllNotesUseCase;
   let mockGetOneNoteUseCase: GetOneNotesUseCase;
 
   beforeAll(() => {
     mockDeleteNoteUseCase = getMockDeleteNoteUseCase();
+    mockCreateNoteUseCase = getMockCreateNoteUseCase();
+    mockUpdateNoteUseCase = getMockUpdateNoteUseCase();
+    mockGetAllNotesUseCase = getMockGetAllNotesUseCase();
+    mockGetOneNoteUseCase = getMockGetOneNoteUseCase();
   });
 
   describe('first', () => {
