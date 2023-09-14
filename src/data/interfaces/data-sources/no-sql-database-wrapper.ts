d59b15a2d943932a828d9edbb7d636ecbe47ebbe
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-14 15:29:32 +0000

    14 Sept 2023, 20:59

--- src/data/interfaces/data-sources/no-sql-database-wrapper.ts
+++ src/data/interfaces/data-sources/no-sql-database-wrapper.ts
@@ -3,5 +3,5 @@ export default interface NoSqlDatabaseWrapper {
   findOne(id: string): Promise<any | null>;
   insertOne(data: object): Promise<any>;
   updateOne(id: string, data: object): Promise<any>;
-  deleteOne(id: string): void;
+  deleteOne(id: string): Promise<void>;
 }
