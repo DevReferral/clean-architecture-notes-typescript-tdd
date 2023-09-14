a43ec488bbd4317e51ea7388372e5cba6c3d7193
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-14 12:50:34 +0000

    14 Sept 2023, 18:20

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import MongoDbNotesDataSource from './data/data-sources/mongodb/mongodb-notes-data-source';
 import Note from './data/data-sources/mongodb/schemas/Note';
 import NoSqlDatabaseWrapper from './data/interfaces/data-sources/nosql-database-wrapper';
+import NotesRepositoryImpl from './domain/repositories/notes-repository';
+import { CreateNote } from './domain/use-cases/create-note';
+import DeleteNote from './domain/use-cases/delete-note';
+import GetAllNotes from './domain/use-cases/get-all-notes';
+import GetOneNote from './domain/use-cases/get-one-note';
+import UpdateNote from './domain/use-cases/update-note';
+import NotesRouter from './presentation/routers/notes-router';
+import server from './server';
 
 import Database from './utils/Database';
 
@@ -27,3 +35,18 @@ async function getMongoDS() {
 
   return new MongoDbNotesDataSource(notesDatabase);
 }
+
+(async () => {
+  const dataSource = await getMongoDS();
+
+  const notesRouter = NotesRouter(
+    new DeleteNote(new NotesRepositoryImpl(dataSource)),
+    new CreateNote(new NotesRepositoryImpl(dataSource)),
+    new UpdateNote(new NotesRepositoryImpl(dataSource)),
+    new GetAllNotes(new NotesRepositoryImpl(dataSource)),
+    new GetOneNote(new NotesRepositoryImpl(dataSource))
+  );
+
+  server.use('/notes', notesRouter);
+  server.listen(4000, () => console.log('Running on http://localhost:4000'));
+})();
