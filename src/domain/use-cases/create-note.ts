a313c85c0516592d7244026b0b85054139811225
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-14 13:49:57 +0000

    14 Sept 2023, 19:19

--- src/domain/use-cases/create-note.ts
+++ src/domain/use-cases/create-note.ts
@@ -5,9 +5,10 @@ import { NotesRequestModel, NotesResponseModel } from '../models/notes';
 export class CreateNote implements CreateNoteUseCase {
   constructor(private readonly notesRepository: NotesRepository) {}
   async execute(note: NotesRequestModel): Promise<NotesResponseModel> {
+    console.log('Note recieved in create note use case', JSON.stringify(note));
     const result = await this.notesRepository.createNote(note);
 
-    console.log('create note use case', JSON.stringify(result));
+    console.log('processed note being sent', JSON.stringify(result));
     return result;
   }
 }
