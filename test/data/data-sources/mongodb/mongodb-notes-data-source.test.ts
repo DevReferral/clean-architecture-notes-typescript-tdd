c8149df256d249c7e2fe08e14f9ad9b873559ba6
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-15 06:43:23 +0000

    15 Sept 2023, 12:13

--- test/data/data-sources/mongodb/mongodb-notes-data-source.test.ts
+++ test/data/data-sources/mongodb/mongodb-notes-data-source.test.ts
@@ -1,5 +1,6 @@
 import MongoDbNotesDataSource from '../../../../src/data/data-sources/mongodb/mongodb-notes-data-source';
 import NoSqlDatabaseWrapper from '../../../../src/data/interfaces/data-sources/no-sql-database-wrapper';
+import { NotesResponseModel } from '../../../../src/domain/models/notes';
 import { getNotesMongoDb } from '../../../helpers/notesMongoDbWrapperHelper';
 
 describe('MongoDb DataSource', () => {
@@ -14,6 +15,23 @@ describe('MongoDb DataSource', () => {
   });
 
   describe('getAll', () => {
-    const db = new MongoDbNotesDataSource(mockDatabase);
+    it('gets all the notes', async () => {
+      const db = new MongoDbNotesDataSource(mockDatabase);
+      const expected: NotesResponseModel[] = [
+        {
+          content: 'a',
+          id: '1',
+          important: true,
+        },
+        {
+          content: 'b',
+          id: '2',
+          important: false,
+        },
+      ];
+      jest
+        .spyOn(mockDatabase, 'find')
+        .mockImplementation(() => Promise.resolve(expected));
+    });
   });
 });
