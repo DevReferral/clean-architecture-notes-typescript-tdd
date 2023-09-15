8395cd5b2358bed123801d03c9a1d9ad74cac798
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-15 06:36:28 +0000

    15 Sept 2023, 12:06

---
+++ test/helpers/notesMongoDbWrapperHelper.ts
@@ -0,0 +1,19 @@
+import NoSqlDatabaseWrapper from '../../src/data/interfaces/data-sources/no-sql-database-wrapper';
+
+export const notesMongoDb: NoSqlDatabaseWrapper = {
+  find: function (query: object): Promise<any[]> {
+    throw new Error('Function not implemented.');
+  },
+  findOne: function (id: string): Promise<any> {
+    throw new Error('Function not implemented.');
+  },
+  insertOne: function (data: object): Promise<any> {
+    throw new Error('Function not implemented.');
+  },
+  updateOne: function (id: string, data: object): Promise<any> {
+    throw new Error('Function not implemented.');
+  },
+  deleteOne: function (id: string): Promise<void> {
+    throw new Error('Function not implemented.');
+  },
+};
