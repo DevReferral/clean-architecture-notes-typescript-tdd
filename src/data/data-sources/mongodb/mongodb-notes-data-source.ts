c7e0a158e6e9cac971de57644ded94010a47deb6
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-15 07:29:19 +0000

    15 Sept 2023, 12:59

--- src/data/data-sources/mongodb/mongodb-notes-data-source.ts
+++ src/data/data-sources/mongodb/mongodb-notes-data-source.ts
@@ -9,7 +9,6 @@ export default class MongoDbNotesDataSource implements NotesDataSource {
   constructor(private readonly db: NoSqlDatabaseWrapper) {}
   async getAll(): Promise<NotesResponseModel[]> {
     const result = await this.db.find({});
-    console.log('The items are ', JSON.stringify(result, null, 2));
     for (let res of result) {
       console.log('id is : ', res._id);
     }
