f471f25066ed7719fe5b096179d49189dc88e298
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-15 06:28:24 +0000

    15 Sept 2023, 11:58

--- test/data/data-sources/mongodb/mongodb-notes-data-source.test.ts
+++ test/data/data-sources/mongodb/mongodb-notes-data-source.test.ts
@@ -1,5 +1,21 @@
-import NoSqlDatabaseWrapper from '../../../../src/data/interfaces/data-soures/nosql-database-wrapper';
+import NoSqlDatabaseWrapper from '../../../../src/data/interfaces/data-sources/no-sql-database-wrapper';
 
 describe('MongoDb DataSource', () => {
-  let mockdatabase: NoSqlDatabaseWrapper;
+  let mockDatabase: NoSqlDatabaseWrapper;
+
+  beforeAll(() => {
+    mockDatabase = {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      insertOne: jest.fn(),
+      updateOne: jest.fn(),
+      deleteOne: jest.fn(),
+    };
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getAll', () => {});
 });
