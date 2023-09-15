010f299939c8c302cf1db3b244e1f94426cec8ef
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-15 06:16:26 +0000

    15 Sept 2023, 11:46

---
+++ test/data/data-sources/mongodb/mongodb-notes-data-source.test.ts
@@ -0,0 +1,5 @@
+import NoSqlDatabaseWrapper from '../../../../src/data/interfaces/data-soures/nosql-database-wrapper';
+
+describe('MongoDb DataSource', () => {
+  let mockdatabase: NoSqlDatabaseWrapper;
+});
